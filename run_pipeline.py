d245946b302e568107204888fd07469b037c762d
Kaustbh <desalekaustubh3@gmail.com>
2024-10-02 17:50:51 +0530

    for running zenml orchestrastion

---
+++ run_pipeline.py
@@ -0,0 +1,5 @@
+from pipelines.training_pipeline import training_pipeline
+
+
+if __name__ == "__main__":
+    training_pipeline(data_path="/home/kaustubh/mlops_projects/Customer_Satifaction/olist_customers_dataset.csv")
